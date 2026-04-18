6df68f398a5730e6dd2d03c780355eb19670eeca
Felipe <felipemonteirose@gmail.com>
2026-04-18 17:35:16 -0300

    sobe versão mínima

--- shared/src/commonMain/kotlin/com/montfel/fipe/ui/home/HomeViewModel.kt
+++ shared/src/commonMain/kotlin/com/montfel/fipe/ui/home/HomeViewModel.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
-open class HomeViewModel() : ViewModel() {
+open class HomeViewModel : ViewModel() {
     private val _uiState = MutableStateFlow(HomeUiState())
     val uiState = _uiState.asStateFlow()
 }
